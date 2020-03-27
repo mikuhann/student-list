ec08ea41856c9da7f7d8df5f0a433aa912d0d3aa
v.aleinik <v.aleinik@godeltech.com>
2020-03-27 12:21:53 +0300

    init redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -10,58 +11,62 @@ import CreateStudentForm from './components/create-student-form';
 import StudentList from './components/student-list';
 import EditStudent from './components/edit-student';
 
+import store from './store';
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Navbar bg='dark' variant='dark'>
-            <Container>
-              <Navbar.Brand>
-                <Link to='/create-student' className='nav-link'>
-                  Student list
-                </Link>
-              </Navbar.Brand>
-              <Nav className='justify-content-end'>
-                <Nav>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Navbar bg='dark' variant='dark'>
+              <Container>
+                <Navbar.Brand>
                   <Link to='/create-student' className='nav-link'>
-                    Create Student
+                    Student list
                   </Link>
+                </Navbar.Brand>
+                <Nav className='justify-content-end'>
+                  <Nav>
+                    <Link to='/create-student' className='nav-link'>
+                      Create Student
+                    </Link>
+                  </Nav>
+                  <Nav>
+                    <Link to='/edit-student/:id' className='nav-link'>
+                      Edit Student
+                    </Link>
+                  </Nav>
+                  <Nav>
+                    <Link to='/student-list' className='nav-link'>
+                      Student List
+                    </Link>
+                  </Nav>
                 </Nav>
-                <Nav>
-                  <Link to='/edit-student/:id' className='nav-link'>
-                    Edit Student
-                  </Link>
-                </Nav>
-                <Nav>
-                  <Link to='/student-list' className='nav-link'>
-                    Student List
-                  </Link>
-                </Nav>
-              </Nav>
-            </Container>
-          </Navbar>
-        </header>
-        <Container className='mt-3'>
-          <Row>
-            <Col md={12}>
-              <div className='wrapper'>
-                <Switch>
-                  <Route exact path='/' component={CreateStudentForm} />
-                  <Route path='/create-student' component={CreateStudentForm} />
-                  <Route path='/edit-student/:id' component={EditStudent} />
-                  <Route path='/student-list' component={StudentList} />
-                </Switch>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </Router>
+              </Container>
+            </Navbar>
+          </header>
+          <Container className='mt-3'>
+            <Row>
+              <Col md={12}>
+                <div className='wrapper'>
+                  <Switch>
+                    <Route exact path='/' component={CreateStudentForm} />
+                    <Route path='/create-student' component={CreateStudentForm} />
+                    <Route path='/edit-student/:id' component={EditStudent} />
+                    <Route path='/student-list' component={StudentList} />
+                  </Switch>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
